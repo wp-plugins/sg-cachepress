8a4a1a8f607637fe9824cccd7fef5bf7b74d3145
Eric Amundson <eric@ivycat.com>
2015-06-11 10:47:12 -0700

    Fix spelling error: SitGround -> SiteGround

--- views/sg-cache.php
+++ views/sg-cache.php
@@ -1,7 +1,7 @@
 <div class="wrap">
 	<div class="box">
 		<h2>SuperCacher for WordPress by SiteGround</h2>		
-		<p>The SuperCacher is a system that allows you to use the SitGround dynamic cache and Memcached to optimize the performance of your WordPress. In order to take advantage of the system you should have the SuperCacher enabled at your web host plus the required cache options turned on below. For more information on the different caching options refer to the <a href="http://www.siteground.com/tutorials/supercacher/" target="_blank">SuperCacher Tutorial</a>! </p>
+		<p>The SuperCacher is a system that allows you to use the SiteGround dynamic cache and Memcached to optimize the performance of your WordPress. In order to take advantage of the system you should have the SuperCacher enabled at your web host plus the required cache options turned on below. For more information on the different caching options refer to the <a href="http://www.siteground.com/tutorials/supercacher/" target="_blank">SuperCacher Tutorial</a>! </p>
 	</div>
 
 	<div class="box clear">
